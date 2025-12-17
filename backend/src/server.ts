ef9220ff73e8a66f08ec1cb94645f7b2c7290ed1
Sujit Pandey <sujitpandey1409@gmail.com>
2025-12-17 23:23:13 +0530

    feat: Update CORS configuration for production environment
    - Updated CORS origin to allow requests from the production URL.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ import taskRoutes from './routes/task.routes';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://taskflow-pro-sooty.vercel.app/login', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
